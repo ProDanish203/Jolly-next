a31f092335fafd7d7cf325f4cd4ec7ed538b0dba
Danish <danishsidd524@gmail.com>
2023-08-24 02:25:46 +0500

    Small ui fix

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
           <main className='flex'>
             <LeftSidebar/>
 
-              <section className='main-container min-h-[200vh] flex flex-col items-center bg-bg px-6 pb-10 pt-28 max-md:pb-32 sm:px-10'>
+              <section className='main-container min-h-screen flex flex-col items-center bg-bg px-6 pb-10 pt-28 max-md:pb-32 sm:px-10'>
                 <div className='w-full max-w-4xl'>
                   {children}
                 </div>
